fc9d25c7b1d63f116436682d5ce2015451348438
Lim Win Sheng <limwinsheng@gmail.com>
2026-01-22 17:04:40 +0800

    Unity setup with basic player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 4f;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.linearVelocity = moveInput * speed;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        Debug.Log("Move input received");
+        moveInput = context.ReadValue<Vector2>();
+    }
+}
