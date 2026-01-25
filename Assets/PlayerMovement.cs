00ddc47b1674be6e2e1606eabe01b925c8551c66
Lim Win Sheng <limwinsheng@gmail.com>
2026-01-25 13:53:03 +0800

    Added player animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,11 +6,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed = 4f;
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator animator;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,7 +23,20 @@ public class PlayerMovement : MonoBehaviour
 
     public void Move(InputAction.CallbackContext context)
     {
-        Debug.Log("Move input received");
+        animator.SetBool("isWalking", true);
+
+        if (context.canceled)
+        {
+            animator.SetBool("isWalking", false);
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
+
         moveInput = context.ReadValue<Vector2>();
+
+        Debug.Log("Move X: " + moveInput.x);
+        animator.SetFloat("InputX", moveInput.x);
+        Debug.Log("Move Y: " + moveInput.y);
+        animator.SetFloat("InputY", moveInput.y);
     }
 }
